fbfeb48731ab6b6f87b2d9ca5015195a82ceaebe
John Chittam <john@jchittam.com>
2020-09-25 16:03:54 -0400

    implemented and tested toUTF32()

--- src/edu/westga/cs3110/unicoder/model/Codepoint.java
+++ src/edu/westga/cs3110/unicoder/model/Codepoint.java
@@ -1,8 +1,31 @@
 package edu.westga.cs3110.unicoder.model;
 
 public class Codepoint {
+	
+	private String hexString;
+	private static final int INTEGER_PARSE_RADIX = 16;
 
 	public Codepoint(String hexString) {
-		// TODO Auto-generated constructor stub
+		this.hexString = hexString.toUpperCase();
+	}
+	
+	/**
+	 * Encodes the codepoint as a UTF-32 string
+	 * 
+	 * @return the encoded UTF-32 string
+	 */
+	public String toUTF32() {
+		int hexStringLength = this.hexString.length();
+		int neededLength = 8;
+		int numberOfZerosToAdd = neededLength - hexStringLength;
+		StringBuilder utf32String = new StringBuilder();
+		
+		for (int i = 0; i < numberOfZerosToAdd; i++) {
+			utf32String.append('0');
+		}
+		
+		utf32String.append(this.hexString);
+		
+		return utf32String.toString();
 	}
 }
---
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestToUTF32.java
@@ -0,0 +1,48 @@
+package edu.westga.cs3110.unicoder.tests.model.codepoint;
+
+import static edu.westga.cs3110.unicoder.tests.utils.AssertionHelper.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+class TestToUTF32 {
+
+	@Test
+	void testCodepointLen6() {
+		Codepoint codepoint = new Codepoint("10FFFE");
+		assertEqualsIgnoreCase("0010fffe", codepoint.toUTF32());
+	}
+	
+	@Test
+	void testCodepointLen5() {
+		Codepoint codepoint = new Codepoint("1f47d");
+		assertEqualsIgnoreCase("0001f47d", codepoint.toUTF32());
+	}
+	
+	@Test
+	void testCodepointLen4() {
+		Codepoint codepoint = new Codepoint("147d");
+		assertEqualsIgnoreCase("0000147d", codepoint.toUTF32());
+	}
+	
+	@Test
+	void testCodepointLen4OneLeading0() {
+		Codepoint codepoint = new Codepoint("0372");
+		assertEqualsIgnoreCase("00000372", codepoint.toUTF32());
+	}
+	
+	@Test
+	void testCodepointLen4TwoLeading0() {
+		Codepoint codepoint = new Codepoint("0072");
+		assertEqualsIgnoreCase("00000072", codepoint.toUTF32());
+	}
+	
+	@Test
+	void testCodepointLen4ThreeLeading0() {
+		Codepoint codepoint = new Codepoint("0002");
+		assertEqualsIgnoreCase("00000002", codepoint.toUTF32());
+	}
+
+}
