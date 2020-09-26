3109496299d42254b2015fff2defbd72067c020d
John Chittam <john@jchittam.com>
2020-09-25 21:09:37 -0400

    adds tests to make sure that non hex can't be passed into the constructor

---
+++ src/edu/westga/cs3110/unicoder/tests/model/codepoint/TestConstructor.java
@@ -0,0 +1,32 @@
+package edu.westga.cs3110.unicoder.tests.model.codepoint;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs3110.unicoder.model.Codepoint;
+
+class TestConstructor {
+
+	@Test
+	void throwsNfeWhenHexStringNull() {
+		assertThrows(NumberFormatException.class, () -> {
+			new Codepoint(null);
+		});
+	}
+	
+	@Test
+	void throwsNfeWhenHexStringContainsNonHex() {
+		assertThrows(NumberFormatException.class, () -> {
+			new Codepoint("GHIKZ");
+		});
+	}
+	
+	@Test
+	void throwsNfeWhenHexStringEmpty() {
+		assertThrows(NumberFormatException.class, () -> {
+			new Codepoint("");
+		});
+	}
+
+}
